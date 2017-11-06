de3e822a760813018d303616bb1aa2d3a865a794
steph <thesqrtminus1@gmail.com>
2017-11-06 15:41:13 -0500

    added startup.sh

---
+++ startup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+xkbcomp xkbmap :0 &
+compton -Ccfb --vsync opengl --backend glx --vsync-aggressive --sw-opti
+sudo conky -c /home/steph/.conkyrc &
+#shutter --min_at_startup &
+discord-canary &
+sudo nohup acpid &
+sleep 10; beautifuldiscord --css /home/steph/discordCSS/css.css
+redshift-gtk &
