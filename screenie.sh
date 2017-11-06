8f0d976984262bdc3f6c30b2c7588a4adc910f3b
steph <thesqrtminus1@gmail.com>
2017-11-06 16:24:16 -0500

    updated screenie.sh

--- screenie.sh
+++ screenie.sh
@@ -4,6 +4,6 @@
 read -r G < <(slop -f "%g");sleep .5
 import -window root -crop $G /tmp/image.png
 xclip -i /dev/null
-xclip -selection clipboard -t image/png -i /tmp/image.png && rm -rf /tmp/image.png0
+xclip -selection clipboard -t image/png -i /tmp/image.png && rm -rf /tmp/image.png
 #rm -rf ~/.cache/shutter/
 
