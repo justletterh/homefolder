fb74bcca217073ca13739a2499c499709c24cce0
steph <thesqrtminus1@gmail.com>
2017-11-23 11:43:04 -0500

    added gitignore and changed startup.sh

--- startup.sh
+++ startup.sh
@@ -3,7 +3,7 @@ xkbcomp xkbmap :0 &
 compton -Ccfb --vsync opengl --backend glx --vsync-aggressive --sw-opti
 sudo conky -c /home/steph/.conkyrc &
 #shutter --min_at_startup &
-discord-canary &
+discord &
 sudo nohup acpid &
 sleep 10; beautifuldiscord --css /home/steph/discordCSS/css.css
-redshift-gtk &
+#redshift-gtk &
