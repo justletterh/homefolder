351d15ceeeca34c41ace4c874700fa7a93526745
steph <thesqrtminus1@gmail.com>
2017-11-24 00:22:57 -0500

    ajdfkaldfk;j

--- screenie.sh
+++ screenie.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-#rm -rf image.png
-#shutter -s -o /tmp/image.png
 read -r G < <(slop -f "%g");sleep .5
 import -window root -crop $G /tmp/image.png
 xclip -i /dev/null
 xclip -selection clipboard -t image/png -i /tmp/image.png && rm -rf /tmp/image.png
-#rm -rf ~/.cache/shutter/
-
