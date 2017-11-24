d23b8f405db7ef0b9ea791c0962095116a032cc2
steph <thesqrtminus1@gmail.com>
2017-11-24 18:48:43 -0500

    updated conkys and added 1 new one

--- startup.sh
+++ startup.sh
@@ -2,8 +2,9 @@
 xkbcomp xkbmap :0 &
 compton -Ccfb --vsync opengl --backend glx --vsync-aggressive --sw-opti
 sudo conky -c /home/steph/.conkyrc &
+sudo conky -c /home/steph/.conkyrc2 &
 #shutter --min_at_startup &
-discord &
+sleep 10; discord-canary &
 sudo nohup acpid &
 sleep 10; beautifuldiscord --css /home/steph/discordCSS/css.css
 #redshift-gtk &
